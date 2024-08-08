5293f863dba6c2efb4561aa57603ac5f4097a9bb
Ramdev <work.ramdev@gmail.com>
2024-08-08 16:03:07 +0530

    linkedList

--- DSA/linkedList.js
+++ DSA/linkedList.js
@@ -8,15 +8,20 @@ class Node {
 class LinkedList {
 	constructor() {
 		this.head = null;
-		this.tail = this.head;
+		this.tail = null;
 		this.length = 0;
 	}
 
 	append(value) {
 		const newNode = new Node(value);
 
-		this.tail.next = newNode;
-		this.tail = newNode;
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
 		this.length++;
 		return this;
 	}
@@ -24,29 +29,31 @@ class LinkedList {
 	prepend(value) {
 		const newNode = new Node(value);
 
-		newNode.next = this.head;
-		this.head = newNode;
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			newNode.next = this.head;
+			this.head = newNode;
+		}
 		this.length++;
 		return this;
 	}
-
 
 	insert(index, value) {
 		if (index >= this.length) {
 			return this.append(value);
 		}
 
-		const newNode = new Node(value);
-
-		if (this.head === null) {
-			this.head = newNode;
-		} else {
-			let currentNode = this.head;
-			while (currentNode.next !== null) {
-				currentNode = currentNode.next
-			}
-			currentNode.next = newNode;
+		if (index === 0) {
+			return this.prepend(value);
 		}
+
+		const newNode = new Node(value);
+		const prevNode = this.traverseToIndex(index - 1);
+		newNode.next = prevNode.next;
+		prevNode.next = newNode;
+
 		this.length++;
 		return this;
 	}
@@ -68,44 +75,48 @@ class LinkedList {
 
 		if (index === 0) {
 			this.head = this.head.next;
+			if (this.length === 1) {
+				this.tail = null;
+			}
 		} else {
 			const prevNode = this.traverseToIndex(index - 1);
 			prevNode.next = prevNode.next.next;
+			if (index === this.length - 1) {
+				this.tail = prevNode;
+			}
 		}
 
 		this.length--;
-		return this
+		return this;
 	}
 
-
 	reverse() {
-		if (!this.head.next) {
-			return this.head;
+		if (!this.head || !this.head.next) {
+			return this;
 		}
-		let first = this.head;
+		let prevNode = null;
+		let currentNode = this.head;
 		this.tail = this.head;
-		let second = first.next;
 
-		while (second) {
-			const temp = second.next;
-			second.next = first;
-			first = second;
-			second = temp;
+		while (currentNode) {
+			const nextNode = currentNode.next;
+			currentNode.next = prevNode;
+			prevNode = currentNode;
+			currentNode = nextNode;
 		}
 
-		this.head.next = null;
-		this.head = first;
-		return this
+		this.head = prevNode;
+		return this;
 	}
 }
 
-let linkedLIst = new LinkedList(10);
-linkedLIst.append(5)
-linkedLIst.append(16)
-linkedLIst.prepend(1)
-linkedLIst.insert(2, 99)
-linkedLIst.insert(20, 88)
-linkedLIst.remove(2)
-linkedLIst.reverse()
+let linkedList = new LinkedList(10);
+linkedList.append(5)
+linkedList.append(16)
+linkedList.prepend(1)
+linkedList.insert(2, 99)
+linkedList.insert(20, 88)
+linkedList.remove(2)
+linkedList.reverse()
 
-cosole.log(linkedLIst, "LL")
+cosole.log(linkedList, "LL")
